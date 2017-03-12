e7896edaa69d1989dd6c20b82787ed4fd6bb911b
Amine Sagaama <amine.sagaama@gmail.com>
2017-03-12 13:05:41 +0100

    removing the side effects

--- src/main/scala/Cafe.scala
+++ src/main/scala/Cafe.scala
@@ -11,4 +11,15 @@ class Cafe {
     (cup, Charge(cc, cup.price))
   }
 
+  case class Charge(cc: CreditCard, amount: Double) {
+
+    def combine(other: Charge): Charge = {
+
+      if (cc == other.cc)
+        Charge(cc, amount + other.amount)
+      else
+        throw new Exception("Can't combine charges to different cards")
+    }
+  }
+
 }
