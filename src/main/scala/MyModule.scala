52f8c94493019ff3e74eb1f5fd1b5e1609ea1546
Amine Sagaama <amine.sagaama@gmail.com>
2017-03-12 19:39:12 +0100

    add fibonacci function

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -4,18 +4,19 @@
 object MyModule {
 
   def main(args: Array[String]): Unit =
-    println(formatAbs(-42))
+  //println(formatAbs(-42))
 
-  println(factorial(5))
+  //println(factorial(5))
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
-  }
+    println(fib(4))
 
-  def abs(n: Int): Int = {
-    if (n < 0) -n
-    else n
+  def fib(n: Int): Int = {
+    def go(n: Int): Int =
+      if (n <= 0 || n <= 1) n
+      else go(n - 1) + go(n - 2)
+
+
+    go(n)
   }
 
   def factorial(n: Int): Int = {
@@ -25,6 +26,16 @@ object MyModule {
 
     go(n, 1)
 
+  }
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  def abs(n: Int): Int = {
+    if (n < 0) -n
+    else n
   }
 
 }
